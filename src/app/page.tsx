d0d447abd463065c3fa6c3a9657ac595774b16ea
Aryan Patel <aryanpatel6215@gmail.com>
2026-04-04 17:53:24 +0530

    1. setup commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="text-3xl">Hello, Next.js!</div>
+    <div className="text-3xl">
+      <p>Hello, Next.js!</p>
+      <Button variant="default">Slick me </Button>
+    </div>
   );
 }
