eca0dd17d7a518e151758c7abe80c42ad521253f
Aryan Patel <aryanpatel6215@gmail.com>
2026-04-12 16:35:39 +0530

    6.feat: implement social authentication, session-based route protection, and home view component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,18 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { authClient } from "@/lib/auth-client";
+import { auth } from "@/lib/auth";
+import HomeView from "@/modules/home/ui/views/home-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
 
-  const onSubmit = async () => {
-    await authClient.signUp.email({
-      email,
-      password,
-      name,
-    },
-      {
-        onError: ({ error }) => {
-          window.alert(error.message)
-        },
-        onSuccess: () => {
-          window.alert("User created successfully")
-        }
-      }
-    )
-
+  if(!session){
+    redirect('/sign-in')
   }
 
-  const onSignIn = async () => {
-    await authClient.signIn.email({
-      email,
-      password,
-    },
-      {
-        onError: ({ error }) => {
-          window.alert(error.message)
-        },
-        onSuccess: () => {
-          window.alert("User signed in successfully")
-        }
-      }
-    )
-
-  }
-
-
-  const { data: session } = authClient.useSession()
-
-  if (session?.user) {
-    return (
-      <div>
-        <h1>Welcome {session.user.name}</h1>
-        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
-      </div>
-    )
-  }
-
-  return (
-    <div className="text-3xl p-4 gap-4">
-      <div>
-
-        <p>Sign Up</p>
-        <Input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <Input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <Button onClick={onSubmit}>Submit</Button>
-      </div>
-      <div>
-
-        <p>Sign In</p>
-        <Input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <Button onClick={onSubmit}>Submit</Button>
-      </div>
-    </div>
-  );
+  return (<HomeView />);
 }
+
+export default Page;
---
+++ src/modules/home/ui/views/home-view.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
+
+
+export default function HomeView() {
+    const router = useRouter()
+  const {data:session} = authClient.useSession()
+  console.log(session)
+
+//   if(!session){
+//     return (
+//       <div>
+//         Loading
+//       </div>
+//     )
+//   } 
+  return (
+      <div>
+        <h1>Logged in as {session?.user?.name}</h1>
+        <Button onClick={() => authClient.signOut({fetchOptions:{
+            onSuccess:() => {
+                router.push('/sign-in')
+            }
+        }})}>Sign Out</Button>
+      </div>
+    )
+}
