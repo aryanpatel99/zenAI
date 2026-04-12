f58916f61022aaf8912d7b1c8fecbf91b0807bde
Aryan Patel <aryanpatel6215@gmail.com>
2026-04-12 14:50:52 +0530

    4.feat: implement authentication system with sign-in and sign-up flows using Drizzle ORM

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,3 @@
-// import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
-// export const usersTable = pgTable("users", {
-//   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-//   name: varchar({ length: 255 }).notNull(),
-//   age: integer().notNull(),
-//   email: varchar({ length: 255 }).notNull().unique(),
-// });
-
-
 import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
